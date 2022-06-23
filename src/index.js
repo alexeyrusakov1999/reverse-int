7c6057d9aa3322cd3b05674a28484d208c6143c6
Alexey Rusakov <alexeyrusakov1999@gmail.com>
2022-06-23 21:35:24 +0300

    reverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n.toString().split("")[0] === "-") {
+        return n.toString().slice(1).split("").reverse().join("");
+    }
+    return Number(n.toString().split("").reverse().join(""));
+};
